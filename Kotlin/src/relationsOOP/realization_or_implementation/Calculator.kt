e9ded8236529cbaef8f4d2b6ca2df30e44b3b270
kelvintandrio <kelvin.tandrio@tsmid.com>
2020-05-12 09:22:45 +0700

    Realization or Implementation

---
+++ Kotlin/src/relationsOOP/realization_or_implementation/Calculator.kt
@@ -0,0 +1,15 @@
+package relationsOOP.realization_or_implementation
+
+class Calculator(override var x: Int, override var y: Int) : CalculatorService {
+    override fun plus(x: Int, y: Int) {
+
+    }
+
+    override fun minus(x: Int, y: Int) {
+
+    }
+
+    override fun times(x: Int, y: Int) {
+        
+    }
+}
---
+++ Kotlin/src/relationsOOP/realization_or_implementation/CalculatorService.kt
@@ -0,0 +1,9 @@
+package relationsOOP.realization_or_implementation
+
+interface CalculatorService {
+    var x: Int
+    var y: Int
+    fun plus(x: Int, y: Int)
+    fun minus(x: Int, y: Int)
+    fun times(x: Int, y: Int)
+}
